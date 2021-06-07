14927b49fee2371a7e711a8c4ae96a580535b5bd
Beng05 <2389379229@qq.com>
2021-06-07 23:00:02 +0800

    没搞定

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class QuitGame : MonoBehaviour
 {
     public void Quitgame()
     {
+       
         Application.Quit();
     }
 }
